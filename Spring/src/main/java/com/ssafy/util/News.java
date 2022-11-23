7158376c206c86f1d16662f99f7d26ef55951220
jaeho <pliot123@naver.com>
2022-11-23 17:58:31 +0900

    clawling

--- Spring/src/main/java/com/ssafy/util/News.java
+++ Spring/src/main/java/com/ssafy/util/News.java
@@ -20,6 +20,7 @@ public class News {
 			List<NewsDto> list = new ArrayList<NewsDto>();
 			
 			for(int j=1; j <= 1; j++) {
+				NewsDto newsDto= new NewsDto();
 				String url = "https://news.naver.com/main/list.naver?mode=LS2D&sid2=260&sid1=101&mid=shm&date=20221123&page=" + j;
 				Document doc = Jsoup.connect(url).get();
 				Elements elements = doc.getElementsByAttributeValue("class", "list_body newsflash_body");
@@ -36,16 +37,21 @@ public class News {
 					Element aElement = aElements.get(0);
 //					
 					String articleUrl = aElement.attr("href");		// 기사링크
-					System.out.println(articleUrl);
+//					System.out.println(articleUrl);
+					newsDto.setUrl(articleUrl);
 					
 					Element imgElement = aElement.select("img").get(0);
 					String imgUrl = imgElement.attr("src");			// 사진링크
 					System.out.println(imgUrl);
+					newsDto.setImg(imgUrl);
+					
+					
 					String title = imgElement.attr("alt");			// 기사제목
 					System.out.println(title);
+					newsDto.setSubject(title);
 					
 					String content = doc.getElementsByAttributeValue("class", "lede").get(i).text();
-					
+					newsDto.setContents(contents);
 //					System.out.println(title);
 					System.out.println(content);
 //					System.out.println();
